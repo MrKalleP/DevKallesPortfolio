ae6ad537ca733835fe9d1ef2de8cb1b86d2fa060
kalle pettersson <kallle.v.pettersson@gmail.com>
2025-06-02 15:33:20 +0200

    fixed some picture and styling for about page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ export const Header = () => {
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -70, opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-gradient-to-r from-sky-800/80 to-sky-900/80 shadow-md"
+          className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-gradient-to-r from-slate-600/80 to-slate-700/80 shadow-md"
         >
           <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between text-white">
             <div className="text-xl font-bold tracking-wide">MyPortfolio</div>
@@ -51,7 +51,7 @@ export const Header = () => {
                   key={name}
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.95 }}
-                  className="cursor-pointer hover:text-yellow-300 transition-colors duration-200"
+                  className="cursor-pointer hover:text-orange-500 transition-colors duration-200"
                 >
                   <Link to={path}>{name}</Link>
                 </motion.li>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,36 @@
+import me from '../assets/Me.jpg';
+
 export const About = () => {
   return (
     <>
-      <div className="pt-20">
-        <h2>About</h2>
-      </div>
+      <main className="h-screen pt-20 flex flex-col items-center justify-center">
+        <section className="flex flex-col md:flex-row items-center md:items-start gap-6 mb-6">
+          <img
+            className="h-48 w-64 object-cover mt-3 shadow-md rounded-xl"
+            src={me}
+            alt="picture of me"
+          />
+          <section>
+            <h2 className="text-shadow-5xs text-shadow-sky-300 text-4xl text-center md:text-left">
+              Kalle Pettersson
+            </h2>
+            <p className="text-1m text-center md:text-left mt-2 w-64">
+              I am a joy spreader who is 39 years old and see myself as a future
+              developer, and I am a current student to be a front end developer.
+            </p>
+          </section>
+        </section>
+        <section className="max-w-xl text-center md:text-left">
+          <p className="p-4">
+            With a great interest in making beautiful and user-friendly
+            projects. I am a father and a man with great interest in family. If
+            I have some time to spare, I like to go to the gym or play a game on
+            the computer with some friends, or laze on the sofa with a bag of
+            sweets and a good movie. I have previous experience in warehouse
+            jobs but not in IT, so I am excited to change career direction.
+          </p>
+        </section>
+      </main>
     </>
   );
 };
